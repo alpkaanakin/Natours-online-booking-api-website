df5871e5f700ca0ae20f79771247568b5581236f
mist-magister <alpkaganakin@gmail.com>
2022-05-03 01:01:09 +0300

    fixed stripe bugs-2

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -18,13 +18,13 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
-    display_items: [
+    line_items: [
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: `${req.protocol}://${req.get("host")}/img/tours${
-          tour.imageCover
-        }`,
+        images: [
+          `${req.protocol}://${req.get("host")}/img/tours${tour.imageCover}`,
+        ],
         amount: tour.price * 100,
         currency: "usd",
         quantity: 1,
