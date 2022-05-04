546de7ed4ec12e7058b792e34e55c6bf8e104770
mist-magister <alpkaganakin@gmail.com>
2022-05-04 19:17:54 +0300

    finding payment bugs

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -66,8 +66,10 @@ const webhookCheckout = (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  if (event.type === "checkout.session.completed")
+  if (event.type === "checkout.session.completed") {
+    console.log(event.type);
     createBookingCheckout(event.data.object);
+  }
   res.status(200).json({ received: true });
 };
 
