5b400c844e4bb38dfc012784f0aef783a20a4305
mist-magister <alpkaganakin@gmail.com>
2022-05-04 20:00:34 +0300

    fixed booking bug

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -18,16 +18,23 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
+    mode: "payment",
     line_items: [
       {
-        name: `${tour.name} Tour`,
-        description: tour.summary,
-        images: [
-          `${req.protocol}://${req.get("host")}/img/tours/${tour.imageCover}`,
-        ],
-        amount: tour.price * 100,
-        currency: "usd",
         quantity: 1,
+        price_data: {
+          currency: "usd",
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get("host")}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
+          },
+        },
       },
     ],
   });
@@ -50,7 +57,7 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
 async function createBookingCheckout(session) {
   const tour = session.client_reference_id;
   const user = await User.findOne({ email: session.customer_email }).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 }
 
